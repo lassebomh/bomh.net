1a1b3c73ce651d11a1a2e775c45117a44e6e5fd9
lassebomh <lasse@bomh.net>
2019-12-06 13:51:35 +0100

    Added some example JS

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 
 
+var b = document.getElementById("ball0");
+
+var bx = 0;
+var by = 0;
+
+function runFunction() {
+	bx += 1;
+	by += 1;
+	b.style.left = bx.toString()+"px";
+	b.style.top = by.toString()+"px";
+}
+
+var t=setInterval(runFunction,1000/60);
+
+
