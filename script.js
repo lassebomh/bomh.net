d29deb70f5fb5149e1b6d75401954c97a6e72260
lassebomh <lasse@bomh.net>
2019-12-07 19:54:44 +0100

    Grav test

--- script.js
+++ script.js
@@ -2,12 +2,23 @@
 
 var b = document.getElementById("ball0");
 
-var bx = 0;
-var by = 0;
+var bx = 400;
+var by = 400;
+
+var vbx = 0;
+var vby = 0;
+
+var gx = 400;
+var gy = 800;
 
 function runFunction() {
-	bx += 1;
-	by += 1;
+	vby += 1;
+	vbx += 0;
+	//vbx += 0.2;
+	//vby += 0.01;
+	bx += vby;
+	by += vbx;
+	console.log(vbx, vby);
 	b.style.left = bx.toString()+"px";
 	b.style.top = by.toString()+"px";
 }
