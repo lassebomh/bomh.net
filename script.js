f3ffa1e50da400b7feefa9a55e75168e31a9f3ac
lassebomh <lasse@bomh.net>
2019-12-07 20:11:32 +0100

    Added bad gravity

--- script.js
+++ script.js
@@ -6,19 +6,17 @@ var bx = 400;
 var by = 400;
 
 var vbx = 0;
-var vby = 0;
+var vby = 2;
 
-var gx = 400;
-var gy = 800;
+var gx = 500;
+var gy = 500;
 
 function runFunction() {
-	vby += 1;
-	vbx += 0;
-	//vbx += 0.2;
-	//vby += 0.01;
-	bx += vby;
-	by += vbx;
-	console.log(vbx, vby);
+	vby += (gy-by)/10000;
+	vbx += (gx-bx)/10000;
+	bx += vbx;
+	by += vby;
+	console.log(by-gy, by-gy);
 	b.style.left = bx.toString()+"px";
 	b.style.top = by.toString()+"px";
 }
