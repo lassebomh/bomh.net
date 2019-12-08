491a92f763b375b2d0096df59cb0e7575691e894
lassebomh <lasse@bomh.net>
2019-12-08 17:58:14 +0100

    Added array of ball objects

--- script.js
+++ script.js
@@ -13,6 +13,20 @@ var gy = 400;
 
 // -(4*gmass*(bx-gx))/((Math.pow((bx-gx), 2)+Math.pow((by-gy)**2), (3/2)))
 
+
+var ObjectsList = [];
+
+for (var i = 0; i < 5; i++) {
+	var obj = document.createElement("img");
+	ObjectsList.push(obj);
+	obj.setAttribute("src", "images/ball.png");
+	obj.setAttribute("id", "ball"+i.toString())
+	obj.className = "ball"
+	document.body.appendChild(obj);
+}
+
+console.log(ObjectsList);
+
 function runFunction() {
 	if (gx != bx && gy != by) {
 		vbx += 1000 * -(4*(bx-gx))/(Math.pow(Math.pow(bx-gx, 2)+Math.pow(by-gy, 2), (3/2)))
@@ -22,7 +36,7 @@ function runFunction() {
 	bx += vbx;
 	by += vby;
 	
-	console.log(by-gy, by-gy);
+	// console.log(by-gy, by-gy);
 	
 	b.style.left = bx.toString()+"px";
 	b.style.top = by.toString()+"px";
