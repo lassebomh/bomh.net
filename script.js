a3d50d311bfb2cd4641c8d6a1eaf1ff815986028
lassebomh <lasse@bomh.net>
2019-12-10 09:55:02 +0100

    Specific planets + unfinished gravity

--- script.js
+++ script.js
@@ -1,51 +1,49 @@
 
+var earth = document.getElementById("earth");
+var sun = document.getElementById("sun");
+var mars = document.getElementById("mars");
+var moon = document.getElementById("moon");
 
-var b = document.getElementById("ball-1");
+var objs = [earth, sun, mars, moon];
+objs.forEach(setupPlanets);
 
-var bx = 800;
-var by = 500;
-
-var vbx = 7;
-var vby = 0;
-
-var gx = 800;
-var gy = 400;
-
-// -(4*gmass*(bx-gx))/((Math.pow((bx-gx), 2)+Math.pow((by-gy)**2), (3/2)))
-
-var ObjectsList = [];
-
-for (var i = 0; i < 2; i++) {
-	var obj = document.createElement("img");
-	obj.setAttribute("src", "images/ball.png");
-	obj.setAttribute("id", "ball"+i.toString());
-	obj.className = "ball";
-	obj.style.top = "400px";
-	obj.style.left = "400px";
-	document.body.appendChild(obj);
-	ObjectsList.push(obj);
+function setupPlanets(item, index) {
+	item.mass = [1, 1, 1, 1][index]
+	item.pos_x = 400+Math.random()*1000;
+	item.pos_y = 100+Math.random()*700;
+	item.dx = 0;
+	item.dy = 0;
 }
 
-function runFunction() {
+// 	obj.style.top = "400px";
+// 	obj.style.left = "400px";
 
-	for (var i = 0; i < ObjectsList.length; i++){
-		console.log(ObjectsList[i].style.left);
+function tick() {
+
+	function iter_obj_0(self_obj, index) {
+		// console.log("---------");
+		function iter_obj_1(other_obj, index) {
+			
+			if (self_obj == other_obj) {
+				return;
+			}
+			if (!(self_obj.pos_x == other_obj.pos_x && self_obj.pos_y == other_obj.pos_y)) {
+				self_obj.dx += 0.0001 * -(other_obj.mass*(self_obj.pos_x-other_obj.pos_x))/((Math.pow(self_obj.pos_x-other_obj.pos_x, 2)+Math.pow(self_obj.pos_y-other_obj.pos_y, 2), (3/2)));
+				self_obj.dy += 0.0001 * -(other_obj.mass*(self_obj.pos_y-other_obj.pos_y))/((Math.pow(self_obj.pos_x-other_obj.pos_x, 2)+Math.pow(self_obj.pos_y-other_obj.pos_y, 2), (3/2)));
+			}
+		}
+		objs.forEach(iter_obj_1);
+	}
+	objs.forEach(iter_obj_0);
+
+	function set_obj_x(item, index) {
+		item.pos_x += item.dx;
+		item.pos_y += item.dy;
+		item.style.left = (item.pos_x).toString()+"px";
+		item.style.top = (item.pos_y).toString()+"px";
 	}
 
-	if (gx != bx && gy != by) {
-		vbx += 1000 * -(4*(bx-gx))/(Math.pow(Math.pow(bx-gx, 2)+Math.pow(by-gy, 2), (3/2)))
-		vby += 1000 * -(4*(by-gy))/(Math.pow(Math.pow(bx-gx, 2)+Math.pow(by-gy, 2), (3/2)))
-	}
-
-	bx += vbx;
-	by += vby;
-	
-	// console.log(by-gy, by-gy);
-	
-	b.style.left = bx.toString()+"px";
-	b.style.top = by.toString()+"px";
+	objs.forEach(set_obj_x);
 }
 
-var t=setInterval(runFunction,1000/60);
-
-
+var t=setInterval(tick, 1000/60);
