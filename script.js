63298cb58592468db90d137f93b62abdaa63e160
lassebomh <lasse@bomh.net>
2019-12-10 10:15:22 +0100

    Fixed gravity

--- script.js
+++ script.js
@@ -5,14 +5,25 @@ var mars = document.getElementById("mars");
 var moon = document.getElementById("moon");
 
 var objs = [earth, sun, mars, moon];
+
+var centerpos = [600, 400];
+var centerdist = 400;
+
+// .mass = ;
+// .pos_x = ;
+// .pos_y = ;
+// .dx = ;
+// .dy = ;
+
+
 objs.forEach(setupPlanets);
 
 function setupPlanets(item, index) {
-	item.mass = [1, 1, 1, 1][index]
-	item.pos_x = 400+Math.random()*1000;
-	item.pos_y = 100+Math.random()*700;
-	item.dx = 0;
-	item.dy = 0;
+	item.mass = [1, 4, 1, 1][index];
+	item.pos_x = 900+Math.random()*300-150;
+	item.pos_y = 400+Math.random()*200-100;
+	item.dx = Math.random()*4-2;
+	item.dy = Math.random()*4-2;
 }
 
 // 	obj.style.top = "400px";
@@ -28,8 +39,8 @@ function tick() {
 				return;
 			}
 			if (!(self_obj.pos_x == other_obj.pos_x && self_obj.pos_y == other_obj.pos_y)) {
-				self_obj.dx += 0.0001 * -(other_obj.mass*(self_obj.pos_x-other_obj.pos_x))/((Math.pow(self_obj.pos_x-other_obj.pos_x, 2)+Math.pow(self_obj.pos_y-other_obj.pos_y, 2), (3/2)));
-				self_obj.dy += 0.0001 * -(other_obj.mass*(self_obj.pos_y-other_obj.pos_y))/((Math.pow(self_obj.pos_x-other_obj.pos_x, 2)+Math.pow(self_obj.pos_y-other_obj.pos_y, 2), (3/2)));
+				self_obj.dx += 0.00001 * -(other_obj.mass*(self_obj.pos_x-other_obj.pos_x))/((Math.pow(self_obj.pos_x-other_obj.pos_x, 2)+Math.pow(self_obj.pos_y-other_obj.pos_y, 2), (3/2)));
+				self_obj.dy += 0.00001 * -(other_obj.mass*(self_obj.pos_y-other_obj.pos_y))/((Math.pow(self_obj.pos_x-other_obj.pos_x, 2)+Math.pow(self_obj.pos_y-other_obj.pos_y, 2), (3/2)));
 			}
 		}
 		objs.forEach(iter_obj_1);
