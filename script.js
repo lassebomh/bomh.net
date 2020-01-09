95bc0b5ac86abc20f4b3d2e4f7893ecb94eab45c
Lasse Holmgaard Bomholt <lasse@bomh.net>
2020-01-09 16:01:58 +0100

    Darkmode

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ window.onresize = function(event) {
 
 	// }
 	// else
-	if (window.innerWidth < 1250) {
+	if (window.innerWidth < 1250 || window.innerWidth/window.innerHeight > 1) {
 		pf_div.style.marginLeft = (window.innerWidth/2 - 600/2).toString()+"px";
 		arrow_img.style.visibility = 'hidden';
 	}
 	else {
-		pf_div.style.marginLeft = (window.innerWidth/2 - 900/2).toString()+"px";
+		pf_div.style.marginLeft = (window.innerWidth/2 - 1080/2).toString()+"px";
 		arrow_img.style.visibility = 'visible';
 	}
 };
