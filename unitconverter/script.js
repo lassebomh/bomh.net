e6828b353c792e88f69936a86b985cfc56179fef
Lasse Holmgaard Bomholt <lasse@bomh.net>
2020-01-11 22:49:42 +0100

    Added unit converter

---
+++ unitconverter/script.js
@@ -0,0 +1,8 @@
+
+outputArea = document.getElementById('outputarea');
+inputArea = document.getElementById('inputarea');
+
+function masterConvert() {
+	console.log("Heyooo");
+	outputArea.value = inputArea.value;
+}
