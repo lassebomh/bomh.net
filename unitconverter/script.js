d2a2612641245ad3b2799b98dd6734795dc16bab
Lasse Holmgaard Bomholt <lasse@bomh.net>
2020-04-23 20:34:07 +0200

    flems

--- unitconverter/script.js
+++
@@ -1,8 +0,0 @@
-
-outputArea = document.getElementById('outputarea');
-inputArea = document.getElementById('inputarea');
-
-function masterConvert() {
-	console.log("Heyooo");
-	outputArea.value = inputArea.value;
-}
